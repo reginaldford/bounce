653e74c86f42893ceaa1ab578ac4cb479ccb15f1
Rex <631752+reginaldford@users.noreply.github.com>
2023-11-10 19:45:57 -0500

    No need for msg buffer for enc. Still needed for dec. No mutable key copy. Same quality of randomness

--- src/bounceDec.c
+++ src/bounceDec.c
@@ -15,18 +15,12 @@ unsigned char *bounce_decrypt(unsigned char *msg, unsigned int msgLen, unsigned 
 // Right to left decryption pass
 unsigned char *bounce_decrypt_pass_rl(unsigned char *msg, unsigned int msgLen, unsigned char *key,
                                       unsigned char *output) {
-  // Make a mutable key copy
-  unsigned char mKey[256];
-  for (int i = 0; i < 256; i++)
-    mKey[i] = key[i];
   // Decrypt last byte with unchanged key
   output[msgLen - 1] = msg[msgLen - 1] ^ key[255];
   // Main decryption loop
   for (unsigned int i = msgLen - 2; i + 1 >= 1; i--) {
     // Each iteration uses previously computed output byte as random byte index
-    output[i] = msg[i] ^ mKey[msg[i + 1]];
-    // Key is mutated as we go
-    mKey[msg[i + 1]] = (mKey[mKey[msg[i + 1]]]) + i % 256;
+    output[i] = msg[i] ^ ((key[msg[i + 1]] + i) % 256);
   }
   return output;
 }
@@ -34,18 +28,12 @@ unsigned char *bounce_decrypt_pass_rl(unsigned char *msg, unsigned int msgLen, u
 // Left to right decryption pass
 unsigned char *bounce_decrypt_pass_lr(unsigned char *msg, unsigned int msgLen, unsigned char *key,
                                       unsigned char *output) {
-  // Make a mutable key copy
-  unsigned char mKey[256];
-  for (int i = 0; i < 256; i++)
-    mKey[i] = key[i];
   // Decrypt first byte with unchanged key
   output[0] = msg[0] ^ key[0];
   // Main decryption loop
   for (unsigned int i = 1; i <= msgLen - 1; i++) {
     // Each iteration uses previously computed output byte as random byte index
-    output[i] = msg[i] ^ mKey[msg[i - 1]];
-    // Key is mutated as we go
-    mKey[msg[i - 1]] = (mKey[mKey[msg[i - 1]]]) + i % 256;
+    output[i] = msg[i] ^ ((key[msg[i - 1]] + i) % 256);
   }
   return output;
 }
--- src/bounceEnc.c
+++ src/bounceEnc.c
@@ -5,28 +5,21 @@
 // It takes 2
 unsigned char *bounce_encrypt(unsigned char *msg, unsigned int msgLen, unsigned char *key,
                               unsigned char *output) {
-  unsigned char *buffer = malloc(msgLen);
-  bounce_encrypt_pass_rl(msg, msgLen, key, buffer);
-  bounce_encrypt_pass_lr(buffer, msgLen, key, output);
-  free(buffer);
+  bounce_encrypt_pass_rl(msg, msgLen, key, output);
+  bounce_encrypt_pass_lr(output, msgLen, key, output);
   return output;
 }
 
 // Right to left encryption pass
 unsigned char *bounce_encrypt_pass_rl(unsigned char *msg, unsigned int msgLen, unsigned char *key,
                                       unsigned char *output) {
-  // Make a mutable key copy
-  unsigned char mKey[256];
-  for (int i = 0; i < 256; i++)
-    mKey[i] = key[i];
   // Last output byte is input ^ (random byte)
   output[msgLen - 1] = msg[msgLen - 1] ^ key[255];
   // Main encryption loop
   for (unsigned int i = msgLen - 2; i + 1 >= 1; i--) {
-    // Each iteration uses previous output byte as random byte index
-    output[i] = msg[i] ^ mKey[output[i + 1]];
-    // Key is mutated as we go
-    mKey[output[i + 1]] = (mKey[mKey[output[i + 1]]]) + i % 256;
+    // Each iteration uses previously computed output byte as random byte index
+    // Add i into the index to destroy bounce cycles
+    output[i] = msg[i] ^ ((key[output[i + 1]] + i) % 256);
   }
   return output;
 }
@@ -34,18 +27,13 @@ unsigned char *bounce_encrypt_pass_rl(unsigned char *msg, unsigned int msgLen, u
 // Left to right encryption pass
 unsigned char *bounce_encrypt_pass_lr(unsigned char *msg, unsigned int msgLen, unsigned char *key,
                                       unsigned char *output) {
-  // Make a mutable key copy
-  unsigned char mKey[256];
-  for (int i = 0; i < 256; i++)
-    mKey[i] = key[i];
   // First output byte is input ^ (random byte)
   output[0] = msg[0] ^ key[0];
   // Main encryption loop
   for (unsigned int i = 1; i <= msgLen - 1; i++) {
-    // Each iteration uses previous output byte as random byte index
-    output[i] = msg[i] ^ mKey[output[i - 1]];
-    // Key is mutated as we go
-    mKey[output[i - 1]] = (mKey[mKey[output[i - 1]]]) + i % 256;
+    // Each iteration uses previously computed output byte as random byte index
+    // Add i into the index to destroy bounce cycles
+    output[i] = msg[i] ^ ((key[output[i - 1]] + i) % 256);
   }
   return output;
 }
